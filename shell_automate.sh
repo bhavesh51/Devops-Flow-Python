89cf0956fef2624106de694f9f6b28173d516b6d
bhavesh <bhavesh@perfectvips.com>
2021-07-02 17:48:43 +0530

    update file

--- shell_automate.sh
+++ shell_automate.sh
@@ -1,5 +1,5 @@
 minikube start
 eval $(minikube -p minikube docker-env)
 docker build -f Dockerfile -t python-docker:latest .
-kubectl delete -f /home/bhavesh/Desktop/Devops-Flow-Python-main/deployment.yaml
-kubectl create -f /home/bhavesh/Desktop/Devops-Flow-Python-main/deployment.yaml
+kubectl delete -f /home/pvips-guest/Desktop/Devops_Flow/Devops-Flow-Python/deployment.yaml 
+kubectl create -f /home/pvips-guest/Desktop/Devops_Flow/Devops-Flow-Python/deployment.yaml
